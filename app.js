7e841b998ff526a0416146c7460f6010f4fade18
luccamicale <luccamicale2015@gmail.com>
2022-09-13 10:43:57 -0300

    I changed the mistake

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const hamburger = document.querySelector('.menu');
 const overlay = document.querySelector('.overlay');
 const menumain = document.querySelector('.menu-main');
 const cancel = document.querySelector('.cancel');
+const linkss = document.querySelectorAll('.menu-cancel');
 
 hamburger.addEventListener('click', () => {
-  menumain.style.display = 'flex';
   overlay.style.display = 'block';
 });
 
 cancel.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
+
+for (let i = 0; i < linkss.length; i += 1) {
+  linkss[i].addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
+}
