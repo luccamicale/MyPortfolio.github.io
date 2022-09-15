aabe3018dab6cfe68f2d3f0df57ef0cfc9d143fe
luccamicale <luccamicale2015@gmail.com>
2022-09-15 16:15:39 -0300

    Changes in js

--- app.js
+++ app.js
@@ -19,164 +19,144 @@ for (let i = 0; i < linkss.length; i += 1) {
   });
 }
 
-//-- start with the popup window --//
-
 const projects = [
-    {
-      projectId: 'project-1',
-      projectName: 'Tonic',
-      projectClasses: [
-        'CANOPY',
-        '• Back End Dev',
-        '• 2015',
-      ],
-      projectImage: './images/SnapshootPortfolio(2).jpg',
-      languages: [
-        'CSS',
-        'HTML',
-        'Bootsrap',
-        'Ruby',
-        'GitHub',
-        'Javascript',
-      ],
-      liveLink: 'See Live',
-      sourceLink: 'See Source',
-      description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
-    },
-    {
-        projectId: 'project-2',
-        projectName: 'My Post Stories',
-        projectClasses: [
-          'CANOPY',
-          '• Back End Dev',
-          '• 2015',
-        ],
-        projectImage: './images/Snapshoot123Portfolio.png',
-        languages: [
-          'CSS',
-          'HTML',
-          'Bootsrap',
-          'Ruby',
-          'GitHub',
-          'Javascript',
-        ],
-        liveLink: 'See Live',
-        sourceLink: 'See Source',
-        description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
-      },
-      {
-        projectId: 'project-3',
-        projectName: 'Tonic',
-        projectClasses: [
-          'CANOPY',
-          '• Back End Dev',
-          '• 2015',
-        ],
-        projectImage: './images/SnapshootPortfolio77.jpg',
-        languages: [
-          'CSS',
-          'HTML',
-          'Bootsrap',
-          'Ruby',
-          'GitHub',
-          'Javascript',
-        ],
-        liveLink: 'See Live',
-        sourceLink: 'See Source',
-        description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
-      },
-      {
-        projectId: 'project-4',
-        projectName: 'My Post stories',
-        projectClasses: [
-          'CANOPY',
-          '• Back End Dev',
-          '• 2015',
-        ],
-        projectImage: './images/SnapshootPortfolio2).jpg',
-        languages: [
-          'CSS',
-          'HTML',
-          'Bootsrap',
-          'Ruby',
-          'GitHub',
-          'Javascript',
-        ],
-        liveLink: 'See Live',
-        sourceLink: 'See Source',
-        description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
-      },
+  {
+    projectId: 'project-1',
+    projectName: 'Tonic',
+    projectClasses1: 'CANOPY',
+    projectClasses2: '• Back End Dev',
+    projectClasses3: '• 2015',
+    projectImage: './images/SnapshootPortfolio(2).jpg',
+    languages1: 'CSS',
+    languages2: 'HTML',
+    languages3: 'Bootsrap',
+    languages4: 'Ruby',
+    languages5: 'GitHub',
+    languages6: 'Javascript',
+    liveLink: 'See Live',
+    sourceLink: 'See Source',
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
+  },
+  {
+    projectId: 'project-2',
+    projectName: 'My Post Stories',
+    projectClasses1: 'CANOPY',
+    projectClasses2: '• Back End Dev',
+    projectClasses3: '• 2015',
+    projectImage: './images/Snapshoot123Portfolio.png',
+    languages1: 'CSS',
+    languages2: 'HTML',
+    languages3: 'Bootsrap',
+    languages4: 'Ruby',
+    languages5: 'GitHub',
+    languages6: 'Javascript',
+    liveLink: 'See Live',
+    sourceLink: 'See Source',
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
+  },
+  {
+    projectId: 'project-3',
+    projectName: 'Tonic',
+    projectClasses1: 'CANOPY',
+    projectClasses2: '• Back End Dev',
+    projectClasses3: '• 2015',
+    projectImage: './images/SnapshootPortfolio77.jpg',
+    languages1: 'CSS',
+    languages2: 'HTML',
+    languages3: 'Bootsrap',
+    languages4: 'Ruby',
+    languages5: 'GitHub',
+    languages6: 'Javascript',
+    liveLink: 'See Live',
+    sourceLink: 'See Source',
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
+  },
+  {
+    projectId: 'project-4',
+    projectName: 'My Post stories',
+    projectClasses1: 'CANOPY',
+    projectClasses2: '• Back End Dev',
+    projectClasses3: '• 2015',
+    projectImage: './images/SnapshootPortfolio2).jpg',
+    languages1: 'CSS',
+    languages2: 'HTML',
+    languages3: 'Bootsrap',
+    languages4: 'Ruby',
+    languages5: 'GitHub',
+    languages6: 'Javascript',
+    liveLink: 'See Live',
+    sourceLink: 'See Source',
+    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita. ",
+  },
 ]
-   
-    document.getElementById('button-see').addEventListener('click', () => {
-      document.getElementById('modal-container').style.display = 'block';
-      document.getElementById('project-name').textContent = projects[0].projectName;
-      document.getElementById('canopy-modal').textContent= projects[0].projectClasses[0];
-      document.getElementById('back-modal').textContent = projects[0].projectClasses[1];
-      document.getElementById('year-modal').textContent = projects[0].projectClasses[2];
-      document.getElementById('project-image').src = projects[0].projectImage;
-      document.getElementById('html-modal').textContent = projects[0].languages[0];
-      document.getElementById('css-modal').textContent = projects[0].languages[1];
-      document.getElementById('bootstrap-moda1').textContent = projects[0].languages[2];
-      document.getElementById('ruby-modal').textContent = projects[0].languages[3];
-      document.getElementById('java-modal').textContent = projects[0].languages[4];
-      document.getElementById('github-modal').textContent = projects[0].languages[5];
-      document.getElementById('live-link1').textContent = projects[0].liveLink[0];
-      document.getElementById('source-link1').textContent = projects[0].sourceLink[0];
-      document.getElementById('modal-description').textContent = projects[0].description[0];
-    });
-    document.getElementById('button-see-two').addEventListener('click', () => {
-      document.getElementById('modal-container').style.display = 'block';
-      document.getElementById('project-name').textContent = projects[1].projectName;
-      document.getElementById('canopy-modal').textContent = projects[1].projectClasses[0];
-      document.getElementById('back-modal').textContent = projects[1].projectClasses[1];
-      document.getElementById('year-modal').textContent = projects[1].projectClasses[2];
-      document.getElementById('project-image').src = projects[1].projectImage;
-      document.getElementById('html-modal').textContent = projects[1].languages[0];
-      document.getElementById('css-modal').textContent = projects[1].languages[1];
-      document.getElementById('bootstrap-moda1').textContent = projects[1].languages[2];
-      document.getElementById('ruby-modal').textContent = projects[1].languages[3];
-      document.getElementById('java-modal').textContent = projects[1].languages[4];
-      document.getElementById('github-modal').textContent = projects[1].languages[5];
-      document.getElementById('live-link1').textContent = projects[1].liveLink[0];
-      document.getElementById('source-link1').textContent = projects[1].sourceLink[0];
-      document.getElementById('modal-description').textContent = projects[1].description[0];
-    });
-    document.getElementById('button-tonic-3').addEventListener('click', () => {
-      document.getElementById('modal-container').style.display = 'block';
-      document.getElementById('project-name').textContent = projects[2].projectName;
-      document.getElementById('canopy-modal').textContent = projects[2].projectClasses[0];
-      document.getElementById('back-modal').textContent = projects[2].projectClasses[1];
-      document.getElementById('year-modal').textContent = projects[2].projectClasses[2];
-      document.getElementById('project-image').src= projects[2].projectImage;
-      document.getElementById('html-modal').textContent = projects[2].languages[0];
-      document.getElementById('css-modal').textContent = projects[2].languages[1];
-      document.getElementById('bootstrap-moda1').textContent = projects[2].languages[2];
-      document.getElementById('ruby-modal').textContent = projects[2].languages[3];
-      document.getElementById('java-modal').textContent = projects[2].languages[4];
-      document.getElementById('github-modal').textContent = projects[2].languages[5];
-      document.getElementById('live-link').textContent = projects[2].liveLink[0];
-      document.getElementById('source-link').textContent = projects[2].sourceLink[0];
-      document.getElementById('modal-description').textContent = projects[2].description[0];
-    });
-    document.getElementById('button-see-mypost').addEventListener('click', () => {
-      document.getElementById('modal-container').style.display = 'block';
-      document.getElementById('project-name').textContent = projects[3].projectName;
-      document.getElementById('canopy-modal').textContent = projects[3].projectClasses[0];
-      document.getElementById('back-modal').textContent = projects[3].projectClasses[1];
-      document.getElementById('year-modal').textContent = projects[3].projectClasses[2];
-      document.getElementById('project-image').src = projects[3].projectImage;
-      document.getElementById('html-modal').textContent = projects[3].languages[0];
-      document.getElementById('css-modal').textContent = projects[3].languages[1];
-      document.getElementById('bootstrap-moda1').textContent = projects[3].languages[2];
-      document.getElementById('ruby-modal').textContent = projects[3].languages[3];
-      document.getElementById('java-modal').textContent = projects[3].languages[4];
-      document.getElementById('github-modal').textContent = projects[3].languages[5];
-      document.getElementById('live-link1').textContent = projects[3].liveLink[0];
-      document.getElementById('source-link1').textContent = projects[3].sourceLink[0];
-      document.getElementById('modal-description').textContent = projects[3].description[0];
-    });
-
-    document.getElementById('modal-close').addEventListener('click', () => {
-      document.getElementById('modal-container').style.display = 'none';
-      });
+document.getElementById('button-see').addEventListener('click', () => {
+  document.getElementById('modal-container').style.display = 'block';
+  document.getElementById('project-name').textContent = projects[0].projectName;
+  document.getElementById('canopy-modal').textContent= projects[0].projectClasses1;
+  document.getElementById('back-modal').textContent = projects[0].projectClasses2;
+  document.getElementById('year-modal').textContent = projects[0].projectClasses3;
+  document.getElementById('project-image').src = projects[0].projectImage;
+  document.getElementById('html-modal').textContent = projects[0].languages1;
+  document.getElementById('css-modal').textContent = projects[0].languages2;
+  document.getElementById('bootstrap-moda1').textContent = projects[0].languages3;
+  document.getElementById('ruby-modal').textContent = projects[0].languages4;
+  document.getElementById('java-modal').textContent = projects[0].languages5;
+  document.getElementById('github-modal').textContent = projects[0].languages6;
+  document.getElementById('live-link1').textContent = projects[0].liveLink;
+  document.getElementById('source-link1').textContent = projects[0].sourceLink;
+  document.getElementById('modal-description').textContent = projects[0].description;
+});
+document.getElementById('button-see-two').addEventListener('click', () => {
+  document.getElementById('modal-container').style.display = 'block';
+  document.getElementById('project-name').textContent = projects[1].projectName;
+  document.getElementById('canopy-modal').textContent = projects[1].projectClasses1;
+  document.getElementById('back-modal').textContent = projects[1].projectClasses2;
+  document.getElementById('year-modal').textContent = projects[1].projectClasses3;
+  document.getElementById('project-image').src = projects[1].projectImage;
+  document.getElementById('html-modal').textContent = projects[1].languages1;
+  document.getElementById('css-modal').textContent = projects[1].languages2;
+  document.getElementById('bootstrap-moda1').textContent = projects[1].languages3;
+  document.getElementById('ruby-modal').textContent = projects[1].languages4;
+  document.getElementById('java-modal').textContent = projects[1].languages5;
+  document.getElementById('github-modal').textContent = projects[1].languages6;
+  document.getElementById('live-link1').textContent = projects[1].liveLink;
+  document.getElementById('source-link1').textContent = projects[1].sourceLink;
+  document.getElementById('modal-description').textContent = projects[1].description;
+});
+document.getElementById('button-tonic-3').addEventListener('click', () => {
+  document.getElementById('modal-container').style.display = 'block';
+  document.getElementById('project-name').textContent = projects[2].projectName;
+  document.getElementById('canopy-modal').textContent = projects[2].projectClasses1;
+  document.getElementById('back-modal').textContent = projects[2].projectClasses2;
+  document.getElementById('year-modal').textContent = projects[2].projectClasses3;
+  document.getElementById('project-image').src= projects[2].projectImage;
+  document.getElementById('html-modal').textContent = projects[2].languages1;
+  document.getElementById('css-modal').textContent = projects[2].languages2;
+  document.getElementById('bootstrap-moda1').textContent = projects[2].languages3;
+  document.getElementById('ruby-modal').textContent = projects[2].languages4;
+  document.getElementById('java-modal').textContent = projects[2].languages5;
+  document.getElementById('github-modal').textContent = projects[2].languages6;
+  document.getElementById('live-link').textContent = projects[2].liveLink;
+  document.getElementById('source-link').textContent = projects[2].sourceLink;
+  document.getElementById('modal-description').textContent = projects[2].description;
+});
+document.getElementById('button-see-mypost').addEventListener('click', () => {
+  document.getElementById('modal-container').style.display = 'block';
+  document.getElementById('project-name').textContent = projects[3].projectName;
+  document.getElementById('canopy-modal').textContent = projects[3].projectClasses1;
+  document.getElementById('back-modal').textContent = projects[3].projectClasses2;
+  document.getElementById('year-modal').textContent = projects[3].projectClasses3;
+  document.getElementById('project-image').src = projects[3].projectImage;
+  document.getElementById('html-modal').textContent = projects[3].languages1;
+  document.getElementById('css-modal').textContent = projects[3].languages2;
+  document.getElementById('bootstrap-moda1').textContent = projects[3].languages3;
+  document.getElementById('ruby-modal').textContent = projects[3].languages4;
+  document.getElementById('java-modal').textContent = projects[3].languages5;
+  document.getElementById('github-modal').textContent = projects[3].languages6;
+  document.getElementById('live-link1').textContent = projects[3].liveLink;
+  document.getElementById('source-link1').textContent = projects[3].sourceLink;
+  document.getElementById('modal-description').textContent = projects[3].description;
+});
+document.getElementById('modal-close').addEventListener('click', () => {
+  document.getElementById('modal-container').style.display = 'none';
+});
