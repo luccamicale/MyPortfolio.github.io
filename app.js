7bf328302a6114d912848b11d85a38801afe3053
luccamicale <luccamicale2015@gmail.com>
2022-09-16 12:21:06 -0300

    I changed all things bad in my linters

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ const mail = document.getElementById('mail');
 const form = document.getElementById('form');
 const word = document.getElementById('mistake');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
   if (mail.value === mail.value.toLowerCase()) {
     word.textContent = '';
   } else {
