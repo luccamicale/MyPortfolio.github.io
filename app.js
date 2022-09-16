0666cac324e3cff1a72cd2db552c454b494e3263
luccamicale <luccamicale2015@gmail.com>
2022-09-15 21:54:53 -0300

    New changes in my project

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ const projects = [
     projectClasses2: '• Back End Dev',
     projectClasses3: '• 2015',
     projectImage: './images/SnapshootPortfolio(2).jpg',
+    projectImage2: './images/IconCancel.png',
+    projectImage3: './images/Icon-Export.png',
+    projectImage4: './images/iconGitHub.png',
     languages1: 'CSS',
     languages2: 'HTML',
     languages3: 'Bootsrap',
@@ -44,6 +47,9 @@ const projects = [
     projectClasses2: '• Back End Dev',
     projectClasses3: '• 2015',
     projectImage: './images/Snapshoot123Portfolio.png',
+    projectImage2: './images/IconCancel.png',
+    projectImage3: './images/Icon-Export.png',
+    projectImage4: './images/iconGitHub.png',
     languages1: 'CSS',
     languages2: 'HTML',
     languages3: 'Bootsrap',
@@ -61,6 +67,9 @@ const projects = [
     projectClasses2: '• Back End Dev',
     projectClasses3: '• 2015',
     projectImage: './images/SnapshootPortfolio77.jpg',
+    projectImage2: './images/IconCancel.png',
+    projectImage3: './images/Icon-Export.png',
+    projectImage4: './images/iconGitHub.png',
     languages1: 'CSS',
     languages2: 'HTML',
     languages3: 'Bootsrap',
@@ -78,6 +87,9 @@ const projects = [
     projectClasses2: '• Back End Dev',
     projectClasses3: '• 2015',
     projectImage: './images/SnapshootPortfolio2).jpg',
+    projectImage2: './images/IconCancel.png',
+    projectImage3: './images/Icon-Export.png',
+    projectImage4: './images/iconGitHub.png',
     languages1: 'CSS',
     languages2: 'HTML',
     languages3: 'Bootsrap',
@@ -96,14 +108,17 @@ document.getElementById('button-see').addEventListener('click', () => {
   document.getElementById('back-modal').textContent = projects[0].projectClasses2;
   document.getElementById('year-modal').textContent = projects[0].projectClasses3;
   document.getElementById('project-image').src = projects[0].projectImage;
+  document.getElementById('photo-cancel').src = projects[0].projectImage2;
+  document.getElementById('photo-git').src = projects[0].projectImage3;
+  document.getElementById('photo-link').src = projects[0].projectImage4;
   document.getElementById('html-modal').textContent = projects[0].languages1;
   document.getElementById('css-modal').textContent = projects[0].languages2;
-  document.getElementById('bootstrap-moda1').textContent = projects[0].languages3;
+  document.getElementById('bootstrap-modal').textContent = projects[0].languages3;
   document.getElementById('ruby-modal').textContent = projects[0].languages4;
   document.getElementById('java-modal').textContent = projects[0].languages5;
   document.getElementById('github-modal').textContent = projects[0].languages6;
-  document.getElementById('live-link1').textContent = projects[0].liveLink;
-  document.getElementById('source-link1').textContent = projects[0].sourceLink;
+  document.getElementById('live-linkl').textContent = projects[0].liveLink;
+  document.getElementById('source-linkl').textContent = projects[0].sourceLink;
   document.getElementById('modal-description').textContent = projects[0].description;
 });
 document.getElementById('button-see-two').addEventListener('click', () => {
@@ -113,14 +128,17 @@ document.getElementById('button-see-two').addEventListener('click', () => {
   document.getElementById('back-modal').textContent = projects[1].projectClasses2;
   document.getElementById('year-modal').textContent = projects[1].projectClasses3;
   document.getElementById('project-image').src = projects[1].projectImage;
+  document.getElementById('photo-cancel').src = projects[1].projectImage2;
+  document.getElementById('photo-git').src = projects[1].projectImage3;
+  document.getElementById('photo-link').src = projects[1].projectImage4;
   document.getElementById('html-modal').textContent = projects[1].languages1;
   document.getElementById('css-modal').textContent = projects[1].languages2;
-  document.getElementById('bootstrap-moda1').textContent = projects[1].languages3;
+  document.getElementById('bootstrap-modal').textContent = projects[1].languages3;
   document.getElementById('ruby-modal').textContent = projects[1].languages4;
   document.getElementById('java-modal').textContent = projects[1].languages5;
   document.getElementById('github-modal').textContent = projects[1].languages6;
-  document.getElementById('live-link1').textContent = projects[1].liveLink;
-  document.getElementById('source-link1').textContent = projects[1].sourceLink;
+  document.getElementById('live-link').textContent = projects[1].liveLink;
+  document.getElementById('source-link').textContent = projects[1].sourceLink;
   document.getElementById('modal-description').textContent = projects[1].description;
 });
 document.getElementById('button-tonic-3').addEventListener('click', () => {
@@ -130,9 +148,12 @@ document.getElementById('button-tonic-3').addEventListener('click', () => {
   document.getElementById('back-modal').textContent = projects[2].projectClasses2;
   document.getElementById('year-modal').textContent = projects[2].projectClasses3;
   document.getElementById('project-image').src = projects[2].projectImage;
+  document.getElementById('photo-cancel').src = projects[2].projectImage2;
+  document.getElementById('photo-git').src = projects[2].projectImage3;
+  document.getElementById('photo-link').src = projects[2].projectImage4;
   document.getElementById('html-modal').textContent = projects[2].languages1;
   document.getElementById('css-modal').textContent = projects[2].languages2;
-  document.getElementById('bootstrap-moda1').textContent = projects[2].languages3;
+  document.getElementById('bootstrap-modal').textContent = projects[2].languages3;
   document.getElementById('ruby-modal').textContent = projects[2].languages4;
   document.getElementById('java-modal').textContent = projects[2].languages5;
   document.getElementById('github-modal').textContent = projects[2].languages6;
@@ -147,14 +168,17 @@ document.getElementById('button-see-mypost').addEventListener('click', () => {
   document.getElementById('back-modal').textContent = projects[3].projectClasses2;
   document.getElementById('year-modal').textContent = projects[3].projectClasses3;
   document.getElementById('project-image').src = projects[3].projectImage;
+  document.getElementById('photo-cancel').src = projects[3].projectImage2;
+  document.getElementById('photo-git').src = projects[3].projectImage3;
+  document.getElementById('photo-link').src = projects[3].projectImage4;
   document.getElementById('html-modal').textContent = projects[3].languages1;
   document.getElementById('css-modal').textContent = projects[3].languages2;
-  document.getElementById('bootstrap-moda1').textContent = projects[3].languages3;
+  document.getElementById('bootstrap-modal').textContent = projects[3].languages3;
   document.getElementById('ruby-modal').textContent = projects[3].languages4;
   document.getElementById('java-modal').textContent = projects[3].languages5;
   document.getElementById('github-modal').textContent = projects[3].languages6;
-  document.getElementById('live-link1').textContent = projects[3].liveLink;
-  document.getElementById('source-link1').textContent = projects[3].sourceLink;
+  document.getElementById('live-link').textContent = projects[3].liveLink;
+  document.getElementById('source-link').textContent = projects[3].sourceLink;
   document.getElementById('modal-description').textContent = projects[3].description;
 });
 document.getElementById('modal-close').addEventListener('click', () => {
